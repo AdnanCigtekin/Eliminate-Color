1b7e7ab9d23f54bdcc62ced3d156cd6080fa697a
AdnanCigtekin <cigtekinadnan@gmail.com>
2018-09-16 01:01:11 +0300

    First release
    Includes:
    -Basic UI
    -Basic functions

---
+++ Project Files/EliminateColor/Form1.cs
@@ -0,0 +1,147 @@
+﻿/*
+    This project is made by Adnan Çığtekin and published to GitHub under MIT License.
+ */
+
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+
+
+namespace EliminateColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path1 = "";
+        Bitmap myBmp;
+   
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        void ProcessFiles(string path,bool flag,string outputPath)
+        {
+           
+            string[] files;
+            string[] directories;
+
+            if (path[path.Length - 1] != '/')
+            {
+                path += "/";
+            }
+
+            files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                string Tempfile = Path.GetFileName(file);
+
+                myBmp = new Bitmap(path + Tempfile);
+                pictureBox1.Image = myBmp;
+                myBmp.MakeTransparent(Color.FromArgb(255, 0, 255));
+                Rectangle rect = new Rectangle(0, 0, myBmp.Width, myBmp.Height);
+                
+                Bitmap output = myBmp.Clone(rect, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                pictureBox1.Image = myBmp;
+
+                string newoutputPath;
+                newoutputPath = outputPath +"/";                
+                string newFileName = newoutputPath + Tempfile;
+
+                myBmp.Save(newFileName, ImageFormat.Png);
+            }
+
+            directories = Directory.GetDirectories(path);
+            foreach (string directory in directories)
+            {
+                // Process each directory recursively
+                string folderName = "";
+                for(int i = directory.Length -1; directory[i] != '/'; i--)
+                {
+                    folderName += directory[i];
+                }
+                folderName = Reverse(folderName);
+                System.IO.Directory.CreateDirectory(outputPath + folderName);
+                ProcessFiles(directory,true, outputPath + folderName);
+            }
+        }
+
+        string GlobalOutputPath = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (path1.Equals(""))
+            {
+                MessageBox.Show("Please select an input folder");
+                return;
+            }
+            if (GlobalOutputPath.Equals(""))
+            {
+                MessageBox.Show("Please select an output folder");
+                return;
+            }
+
+            ProcessFiles(path1,false, GlobalOutputPath);
+            Console.WriteLine("FINISHED");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImage = myBmp;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string sr = folderBrowserDialog1.SelectedPath.ToString();
+                
+                textBox1.Text = sr;
+                //MessageBox.Show("Output has been entered by the user");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string sr = folderBrowserDialog1.SelectedPath.ToString();
+                textBox2.Text = sr;
+                path1 = sr;
+                //MessageBox.Show("Input has been entered by the user");
+
+            }
+        }
+
+        Color colorToBeRemoved= new Color();
+        Bitmap ImcolorToBeRemoved;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ImcolorToBeRemoved = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                colorToBeRemoved = colorDialog1.Color;
+                for (int x = 0; x < ImcolorToBeRemoved.Width; x++)
+                {
+                    for (int y = 0; y < ImcolorToBeRemoved.Height; y++)
+                    {
+                        ImcolorToBeRemoved.SetPixel(x, y, colorToBeRemoved);
+                    }
+                }
+                label4.Visible = false;
+            }
+           
+            pictureBox2.Image = ImcolorToBeRemoved;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Eliminate Color
+
+This project is about replacing the color which was selected by the user with a color with RGBA code (0,0,0,0) which is a transparent color.
+
+This project aims to greatly reduce the upgrade time needed for game companies which are changing their game engine with much more modern one. For example, some of the older game engines use a bright pink color which has and RGB code (255,0,255) to make transparent background for the sprites..etc. If that company wants to use a modern game engine(Let's say Untiy) that company needs to replace that bright pink color with a transparent color which has a RGBA (0,0,0,0). This job can be done with most of the image editors. However, the user needs to open up every image and replace the colors by hand. This job is really time consuming especially that project has 100+ images.(Also this process is prone to human errors like missing some points.) With this little program all of those jobs gets finished in mere seconds.
+
+#Installation
+
+##For non-developers
+	The files you need are in the folder named "Executable". Just download and extract the folder. In the extracted folder there will be a folder named "EliminateColor.exe". Doubleclick that file and you will be good to go.
+
+##For developers
+	The files you need are in the folder named "Project Files". In that file you can open the solution by doubleclicking "EliminateColor.sln".
